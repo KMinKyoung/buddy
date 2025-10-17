87347916d337c33b7f834d27ffe893a5cde0f5e1
MINKYOUNG\kyoun <vldzmehowl32@naver.com>
2025-10-17 15:45:00 +0900

    첫 커밋

---
+++ src/main/java/me/minkyoung/buddy_back/BuddyBackApplication.java
@@ -0,0 +1,13 @@
+package me.minkyoung.buddy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuddyBackApplication.class, args);
+    }
+
+}
